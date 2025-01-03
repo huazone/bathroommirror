4c682bfd3f6cc97da2879609337af480971e73ef
steven.zhang <huazone@hotmail.com>
2025-01-03 10:09:01 +0800

    修改 metadata 菜单

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,86 +3,37 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
+      text: 'Home',
+      href: getPermalink('/')
     },
     {
-      text: 'Pages',
+      text: 'About us',
+      href: getPermalink('/about'),
+    },
+    {
+      text: 'Products',
       links: [
         {
-          text: 'Features (Anchor Link)',
+          text: 'Led bathroom mirror',
           href: getPermalink('/#features'),
         },
         {
-          text: 'Services',
+          text: 'Led Framed mirror',
           href: getPermalink('/services'),
         },
         {
-          text: 'Pricing',
+          text: 'Medicine cabinet mirror',
           href: getPermalink('/pricing'),
         },
         {
-          text: 'About us',
+          text: 'Acrylic lighting mirror',
           href: getPermalink('/about'),
         },
+
         {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
+          text: 'Hollywood  mirror',
           href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
+        }
       ],
     },
     {
@@ -111,9 +62,9 @@ export const headerData = {
       ],
     },
     {
-      text: 'Widgets',
-      href: '#',
-    },
+      text: 'Contact Us',
+      href: getPermalink('/contact'),
+    }
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
